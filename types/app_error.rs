c6f773798d025d7ff5c3f56ff36234e15b47f824
atomoxetine <132525922+atomoxetine@users.noreply.github.com>
2025-03-31 07:06:29 -0300

    improved app_error

--- types/app_error.rs
+++ types/app_error.rs
@@ -1,17 +1,35 @@
+use std::fmt::Display;
+
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
+
+fn cut_trace(trace: &str) -> &str {
+    trace
+        .find("axum::handler::Handler")
+        .and_then(|i| {
+            let temp = trace.get(0..i)?;
+            let idx = temp.rfind('\n')?;
+            trace.get(0..idx)
+        })
+        .unwrap_or("[backtrace unavailable]")
+}
 
 // Make our own error that wraps `anyhow::Error`.
 pub struct AppError(anyhow::Error);
 
+impl Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let source = &self.0;
+        let backtrace = source.backtrace().to_string();
+        let backtrace = cut_trace(backtrace.as_str());
+        write!(f, "INTERNAL SERVER ERROR: {source}. \n{backtrace}")
+    }
+}
+
 // Tell axum how to convert `AppError` into a response.
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
-        )
-            .into_response()
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
     }
 }
 
