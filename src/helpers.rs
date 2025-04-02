7e52db57943012b17d477a7486b68067c4b58499
emily <snri.atomoxetine@proton.me>
2025-04-02 03:02:53 -0300

    added build command

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,14 +1,16 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, bail};
+use std::fs::{self, DirEntry};
+use std::io;
 use std::path::PathBuf;
 use std::process::Command;
 
 /// Looks for Cargo.toml with app = "cheesecake" metadata
-pub fn get_app_dir() -> Option<PathBuf> {
+pub fn get_app_dir() -> anyhow::Result<PathBuf> {
     let cmd = cargo_metadata::MetadataCommand::new();
 
     let metadata = match cmd.exec() {
         Ok(m) => m,
-        Err(_) => return None,
+        Err(_) => bail!("Not in a cheesecake application"),
     };
 
     match metadata
@@ -17,32 +19,26 @@ pub fn get_app_dir() -> Option<PathBuf> {
         .map(|app| app.as_str())
     {
         Some(Some(app)) if app == "cheesecake" => {
-            Some(metadata.workspace_root.into())
+            Ok(metadata.workspace_root.into())
         }
-        _ => None,
+        _ => bail!("Not in a cheesecake application"),
     }
 }
 
 pub fn normalize_dir(command: &str) -> anyhow::Result<Command> {
-    let app_dir = get_app_dir().ok_or(anyhow!("Not in a cheesecake app"))?;
+    let app_dir = get_app_dir()?;
     let mut command = Command::new(command);
     command.current_dir(app_dir.to_str().unwrap());
 
     Ok(command)
 }
 
-pub fn get_task(name: &str) -> Option<String> {
-    match get_app_dir() {
-        Some(_) => (),
-        None => return None,
-    }
+pub fn get_task(name: &str) -> anyhow::Result<String> {
+    let _ = get_app_dir()?;
 
     let cmd = cargo_metadata::MetadataCommand::new();
 
-    let metadata = match cmd.exec() {
-        Ok(m) => m,
-        Err(_) => return None,
-    };
+    let metadata = cmd.exec().unwrap();
 
     metadata
         .workspace_metadata
@@ -51,4 +47,21 @@ pub fn get_task(name: &str) -> Option<String> {
         .flatten()
         .flatten()
         .map(|task| task.to_owned())
+        .ok_or(anyhow!("Task not found"))
+}
+
+pub fn files_recursive(path: PathBuf) -> io::Result<Vec<DirEntry>> {
+    let mut entries = Vec::new();
+
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+
+        if entry.file_type()?.is_dir() {
+            entries.append(&mut files_recursive(entry.path())?);
+        } else {
+            entries.push(entry);
+        }
+    }
+
+    Ok(entries)
 }
